dbd71412fa86f2d9e3ce41e438ea03649bf1d183
Denis Ravinski <delgoden@gmail.com>
2022-06-15 09:41:26 +0300

    completed homework according to the README.md

--- main.go
+++ main.go
@@ -1,14 +1,107 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/delgoden/golang-united-school-homework-8/service"
 	"io"
 	"os"
 )
 
-func Perform(args Arguments, writer io.Writer) error {
+type Arguments map[string]string
+
+var (
+	flagOperationMissing     = errors.New("-operation flag has to be specified")
+	operationCanNotBeHandled = "Operation %s not allowed!"
+	flagFileNameMissing      = errors.New("-fileName flag has to be specified")
+	flagItemNotProvided      = errors.New("-item flag has to be specified")
+	flagIDNotProvided        = errors.New("-id flag has to be specified")
+
+	operations = [4]string{"list", "add", "findById", "remove"}
+)
+
+func parseArgs() Arguments {
+	var (
+		operation string
+		item      string
+		id        string
+		fileName  string
+	)
+
+	args := make(Arguments)
+
+	flag.StringVar(&operation, "operation", "", "")
+	flag.StringVar(&item, "item", "", "")
+	flag.StringVar(&id, "id", "", "")
+	flag.StringVar(&fileName, "fileName", "", "")
+
+	flag.Parse()
+
+	args["operation"] = operation
+	args["fileName"] = fileName
+
+	if item != "" {
+		args["item"] = item
+	}
+
+	if id != "" {
+		args["id"] = id
+	}
+
+	return args
+}
+
+func Perform(args Arguments, writer io.Writer) (err error) {
+
+	if args["operation"] == "" {
+		return flagOperationMissing
+	}
+	if args["fileName"] == "" {
+		return flagFileNameMissing
+	}
+
+	for i, operation := range operations {
+		if args["operation"] == operation {
+			break
+		}
+		if i == len(operations)-1 {
+			return errors.New(fmt.Sprintf(operationCanNotBeHandled, args["operation"]))
+		}
+	}
+
+	switch args["operation"] {
+	case "list":
+		data, err := service.List(args["fileName"])
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprint(writer, data)
+		if err != nil {
+			return err
+		}
+	case "add":
+		if args["item"] == "" {
+			return flagItemNotProvided
+		}
+		err = service.Add(args["item"], args["fileName"], writer)
+	case "findById":
+		if args["id"] == "" {
+			return flagIDNotProvided
+		}
+		err = service.FindById(args["id"], args["fileName"], writer)
+	case "remove":
+		if args["id"] == "" {
+			return flagIDNotProvided
+		}
+		err = service.Remove(args["id"], args["fileName"], writer)
+	}
+
+	return err
 }
 
 func main() {
+
 	err := Perform(parseArgs(), os.Stdout)
 	if err != nil {
 		panic(err)
---
+++ service/service.go
@@ -0,0 +1,170 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	userNotFound      = "Item with id %s not found"
+	itemAlreadyExists = "Item with id %s already exists"
+)
+
+type user struct {
+	Id    string `json:"id"`
+	Email string `json:"email,omitempty"`
+	Age   int    `json:"age,omitempty"`
+}
+
+func List(fileName string) (string, error) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		cerr := file.Close()
+		if cerr != nil {
+			err = cerr
+		}
+	}()
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+
+}
+
+func Add(item string, fileName string, writer io.Writer) error {
+	var userItem user
+	err := json.Unmarshal([]byte(item), &userItem)
+	if err != nil {
+		return err
+	}
+	var users []user
+	itemsInFile, err := List(fileName)
+	if err != nil {
+		return err
+	}
+	if len(itemsInFile) != 0 {
+		err = json.Unmarshal([]byte(itemsInFile), &users)
+		if err != nil {
+			return err
+		}
+
+		for _, user := range users {
+			if user.Id == userItem.Id {
+				_, err = fmt.Fprintf(writer, itemAlreadyExists, userItem.Id)
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+		}
+	}
+
+	users = append(users, userItem)
+
+	data, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	err = os.WriteFile(fileName, []byte(data), 0644)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprint(writer, string(item))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func FindById(id string, fileName string, writer io.Writer) (err error) {
+	var itemFind user
+	itemsInFile, err := List(fileName)
+	if err != nil {
+		return err
+	}
+	var users []user
+
+	if len(itemsInFile) != 0 {
+		err = json.Unmarshal([]byte(itemsInFile), &users)
+		if err != nil {
+			return err
+		}
+
+		for i, user := range users {
+			if user.Id == id {
+				itemFind = user
+
+				break
+			}
+
+			if i == len(users)-1 {
+				_, err = fmt.Fprint(writer, "")
+				if err != nil {
+					return
+				}
+				return
+			}
+		}
+	}
+	data, err := json.Marshal(itemFind)
+	if err != nil {
+		return
+	}
+	_, err = fmt.Fprint(writer, string(data))
+	if err != nil {
+		return
+	}
+	return
+}
+
+func Remove(id string, fileName string, writer io.Writer) (err error) {
+	itemsInFile, err := List(fileName)
+	if err != nil {
+		return err
+	}
+	var users []user
+	if len(itemsInFile) != 0 {
+		err = json.Unmarshal([]byte(itemsInFile), &users)
+		if err != nil {
+			return err
+		}
+
+		for i, user := range users {
+			if user.Id == id {
+				users = append(users[:i], users[i+1:]...)
+				break
+			}
+
+			if i == len(users)-1 {
+				_, err = fmt.Fprintf(writer, userNotFound, id)
+				if err != nil {
+					return
+				}
+			}
+		}
+	}
+
+	data, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	err = os.WriteFile(fileName, []byte(data), 0644)
+	if err != nil {
+		return err
+	}
+
+	return
+}
